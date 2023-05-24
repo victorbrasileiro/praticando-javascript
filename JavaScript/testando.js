ab7f67333ee5b53905dca800e0b22fa6107cdc8b
Victor Brasileiro <71978450+victorbrasileiro@users.noreply.github.com>
2023-05-23 22:43:07 -0400

    praticando logica de programação

---
+++ JavaScript/testando.js
@@ -0,0 +1,8 @@
+let numeroUm = 1
+let stringUm = '1'
+
+if (numeroUm == stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
